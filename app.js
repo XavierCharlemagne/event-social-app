46d6d4c388be30af12ddbd5907b1a4030d8abc34
XavierCharlemagne <xaviercharlemagne728@gmail.com>
2022-03-03 21:29:11 -0600

    db.js and app.js changes

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 "use strict";
+require("dotenv").config();
+
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+const userModel = require("./Models/usersModels")
+
+app.use(express.json());
+
+
+
+app.post("/users", (req, res) =>{
+    res.send("POST /users is under construction");
+});
